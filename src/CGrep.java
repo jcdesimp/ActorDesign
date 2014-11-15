19c733b63a8e788211fb6a253c885843dd97092d
Joseph DeSimpliciis <jcdesimp@aol.com>
2014-11-15 02:07:06 -0500

    skeleton code for main method in Cgrep

--- src/CGrep.java
+++ src/CGrep.java
@@ -1,10 +1,31 @@
 import akka.actor.Actor;
 import akka.actor.TypedActor;
 
+import java.io.File;
+
 public class  CGrep {
 
     public static void main(String[] args) {
-        CollectionActor ca = new CollectionActor();
+
+        // Make sure at least the pattern argument is given
+        if (args.length < 1) {
+            System.out.println("usage: java CGrep pattern [file . . .]");
+            return;
+        }
+
+        // set the pattern for later use
+        String pattern = args[0];
+
+        // Iterate each filename
+        for (int i = 1; i < args.length; i++) {
+            File f = new File(args[i]);
+            if (f.exists()) {
+                // start processing file
+            } else {
+                System.out.println("File " + args[i] + " not found!");
+            }
+        }
+
     }
 }
 
--- src/ScanActor.java
+++ src/ScanActor.java
@@ -2,7 +2,7 @@ import akka.actor.UntypedActor;
 
 /**
  * Created by jcdesimp on 11/14/14.
- * Scans for occurances of a pattern
+ * Scans for occurrences of a pattern in a single file
  */
 public class ScanActor extends UntypedActor {
     private String fileName;
